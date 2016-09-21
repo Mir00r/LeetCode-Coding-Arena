e705a882b49bac058a0e1933861372d9c9357c88
Mir00r <abdur.razzaq06@gmail.com>
2016-09-22 00:09:37 +0600

    sort the array

---
+++ Arrays/Easy/Majority-Element.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define LI long int
+#define LLI long long int
+#define LL __int64
+#define ULL unsigned long long
+#define LLU long long unsigned
+#define row 10
+#define col 10
+#define MAX 10000
+#define jora pair <int, int>
+#define memo(arr, value) memset(arr, value, sizeof(arr))
+#define pb push_back
+#define NL puts ("")
+#define inf (1 << 28)
+#define eps 1e9
+#define MOD 7477777
+#define PI 3.1415926535897932384626433832795
+#define PrimeRange 1000000
+#define CharRange 255
+#define LIMIT 1000
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int len = nums.size();
+        sort (nums.begin(), nums.end() );
+        return nums[ len / 2 ];
+    }
+};
+
