2f113c6f7d24af5dea014f7449fac9916f95398a
Mir00r <abdur.razzaq06@gmail.com>
2016-12-29 01:31:54 +0600

    add new method for pattern from string using KMP algorithm

--- String/Easy/Repeated-Substring-Pattern.cpp
+++ String/Easy/Repeated-Substring-Pattern.cpp
@@ -96,3 +96,47 @@ int main () {
     cout << sol.repeatedSubstringPattern( a );
     return 0;
 }
+
+
+
+// Prints occurrences of txt[] in pat[]
+void KMPSearch(char *pat, char *txt)
+{
+    int M = strlen(pat);
+    int N = strlen(txt);
+
+    // create lps[] that will hold the longest prefix suffix
+    // values for pattern
+    int lps[M];
+
+    // Preprocess the pattern (calculate lps[] array)
+    computeLPSArray(pat, M, lps);
+
+    int i = 0;  // index for txt[]
+    int j  = 0;  // index for pat[]
+    while (i < N)
+    {
+        if (pat[j] == txt[i])
+        {
+            j++;
+            i++;
+        }
+
+        if (j == M)
+        {
+            printf("Found pattern at index %d \n", i-j);
+            j = lps[j-1];
+        }
+
+        // mismatch after j matches
+        else if (i < N && pat[j] != txt[i])
+        {
+            // Do not match lps[0..lps[j-1]] characters,
+            // they will match anyway
+            if (j != 0)
+                j = lps[j-1];
+            else
+                i = i+1;
+        }
+    }
+}
